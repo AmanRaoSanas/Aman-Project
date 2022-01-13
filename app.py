8f57c3283b67f6598e60b0b123f5f15176a39811
AmanRaoSanas <amnsns17@gmail.com>
2022-01-13 19:00:47 +0530

    commit 2 flask task

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,12 +6,27 @@ app = Flask(__name__)
 def index():
     return render_template('Dashboard.html')
 
-@app.route("/login")
-def login():
+@app.route("/log")
+def log():
     return render_template('Login.html')
 
-@app.route("/register")
+@app.route("/login", methods=['POST', "GET"])
+def login():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        print(email,password)
+        return render_template('Dashboard.html', email=email)
+    return render_template('Login.html')
+
+@app.route("/register", methods=['POST', "GET"])
 def register():
+    if request.method == "POST":
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        print(name, email, password)
+        return render_template('Login.html')
     return render_template('Register.html')
 
 if __name__ =="__main__":
